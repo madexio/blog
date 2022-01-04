b80597d430e1e5dcdbb2385e327c17616a6fb0ad
madex <matthewadrienproulx@gmail.com>
2022-01-04 14:21:24 -0800

    Finished video 15 Added findOrFail and find function

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -23,26 +23,34 @@ class Post
         $this->slug    = $slug;
     }
 
+    static function findOrFail($slug)
+    {
+        $post = static::find($slug);
+        if (!$post)
+        {
+            throw new ModelNotFoundException();
+        }
+        return $post;
+    }
     static function find($slug)
     {
-        $posts = static::all();
-        return $posts->firstWhere("slug", $slug);
+        return static::all()->firstWhere("slug", $slug);
     }
 
     static function all()
     {
         return cache()->rememberForever("posts.all", function () {
 
-        return collect(File::files(resource_path("/posts/")))
-            ->map(fn($file) => YamlFrontMatter::parseFile($file))
-            ->map(fn($document) => new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            ))
-            ->sortByDesc("date");
+            return collect(File::files(resource_path("/posts/")))
+                ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))
+                ->sortByDesc("date");
         });
     }
 }
