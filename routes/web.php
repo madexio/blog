f1b35c362a1fd3941ebb1b2a45b91ac79917be5d
madex <matthewadrienproulx@gmail.com>
2022-01-04 11:08:49 -0800

    Vid-1 to 10 because I forgot

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get("posts/{post}", function ($slug) {
+    $path = __DIR__ . "/../resources/posts/$slug.html";
+
+    if (!file_exists($path))
+    {
+        return redirect("/");
+    }
+    $post = cache()->remember("posts.$slug}", 3600, fn() => file_get_contents($path));
+
+    return view("post", [
+        "post" => $post,
+    ]);
+})->where("post", "[A-z_\-]+");
