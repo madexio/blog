d474bd3c3508239e9bcd9ec62d6175dcf4fcacb0
madex <matthewadrienproulx@gmail.com>
2022-01-04 15:17:27 -0800

    Created a second post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
 
         <article>
             <h1>
-                {{$post->title}}
+                {!!$post->title!!}
             </h1>
             <div>
                 {!! $post->body !!}
