ce4266b6b69d804fe3f62473f422ddd6f71b818e
madex <matthewadrienproulx@gmail.com>
2022-01-04 14:14:41 -0800

    Finished video 14, learned a bit about blade components and created a layout component

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,18 +1,15 @@
-<!doctype html>
-
-<title>My Blog</title>
-<link rel="stylesheet"
-      href="/app.css">
-<body>
+<x-layout>
 
     @foreach ($posts as $post)
-    <article>
-        <h1>
-            <a href="/posts/{{$post->slug}}">
-                {{$post->title}}
-            </a>
-        </h1>
-        <div>{{$post->excerpt}}...</div>
-    </article>
+        <article>
+            <h1>
+                <a href="/posts/{{$post->slug}}">
+                    {{$post->title}}
+                </a>
+            </h1>
+            <div>{{$post->excerpt}}...</div>
+        </article>
     @endforeach
-</body>
+
+</x-layout>
+
