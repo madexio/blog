1a5404848c74fbac5af35608f0bc2f70bfb14759
madex <matthewadrienproulx@gmail.com>
2022-01-05 15:37:49 -0800

    Refactored the dropdown to use dropdown-item.blade.php

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -5,7 +5,9 @@
     <div class="py-6 px-5">
         <div>
             {{--TODO--}}
-            <img src="/images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
+            <img src="/images/illustration-3.png"
+                 alt="Blog Post illustration"
+                 class="rounded-xl">
         </div>
 
         <div class="mt-8 flex flex-col justify-between">
@@ -35,7 +37,8 @@
 
             <footer class="flex justify-between items-center mt-8">
                 <div class="flex items-center text-sm">
-                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                    <img src="/images/lary-avatar.svg"
+                         alt="Lary avatar">
                     <div class="ml-3">
                         <h5 class="font-bold">{{$post->author->name}}</h5>
                     </div>
@@ -44,7 +47,8 @@
                 <div>
                     <a href="/posts/{{$post->slug}}"
                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
-                    >Read More</a>
+                    >Read More
+                    </a>
                 </div>
             </footer>
         </div>
