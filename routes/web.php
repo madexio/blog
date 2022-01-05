318d07ba593c3651d004cffbb1c287d8a49c45b1
madex <matthewadrienproulx@gmail.com>
2022-01-05 10:54:06 -0800

    Updated queries to be smarter

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
+use DebugBar\DebugBar;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -20,7 +21,7 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
     return view('posts', [
-        "posts" => Post::latest()->with("category","author")->get(),
+        "posts" => Post::latest()->get()
     ]);
 });
 
