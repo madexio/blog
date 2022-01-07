5927290f69c2a7557417dc9aecdf70f195660ec1
matthewproulx <matthewadrienproulx@gmail.com>
2022-01-07 08:13:17 -0800

    VID-38 Refactored Post to use a PostController.php which can accept filters for the posts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -19,25 +20,9 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 |
 */
 
-Route::get('/', function () {
-    $posts = Post::latest();
+Route::get('/', [PostController::class, "index"])->name("home");
 
-    if (request("search")){
-        $posts->where("title", "like", "%" . request("search") . "%")
-        ->orWhere("excerpt", "like", "%" . request("search") . "%")
-        ->orWhere("body", "like", "%" . request("search") . "%");
-    }
-    return view('posts', [
-        "posts" => $posts->get(),
-        "categories" => Category::all()
-    ]);
-})->name("home");
-
-Route::get("posts/{post:slug}", function (Post $post) {
-    return view("post", [
-        "post" => $post
-    ]);
-});
+Route::get("posts/{post:slug}", [PostController::class, "show"]);
 
 Route::get("categories/{category:slug}", function (Category $category){
     return view ("posts", [
@@ -46,6 +31,7 @@ Route::get("categories/{category:slug}", function (Category $category){
         "categories" => Category::all()
     ]);
 })->name("category");
+
 
 Route::get("authors/{author:username}", function (User $author){
     return view ("posts", [
