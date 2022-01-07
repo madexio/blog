2fe1328abd9e25fbd0a5cfcda7049ab5eaea69f2
matthewproulx <matthewadrienproulx@gmail.com>
2022-01-07 15:59:39 -0800

    VID-50 Added log in page and functionality

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,9 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view("sessions.create");
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            "email"    => ["required", "email"],
+            "password" => ["required"],
+        ]);
+
+        if (auth()->attempt($attributes))
+        {
+            session()->regenerate();
+            return redirect("/")->with("success", "Logged in successfully");
+        }
+
+        throw ValidationException::withMessages([
+            "email"=>"Your provided credentials could not be validated"
+        ]);
+    }
+
     public function destroy()
     {
         auth()->logout();
