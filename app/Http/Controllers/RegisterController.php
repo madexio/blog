ad87f473aae78a1ea47f4469b65e576699d63ed6
matthewproulx <matthewadrienproulx@gmail.com>
2022-01-07 13:07:43 -0800

    VID-46 encrypted passwords

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -14,12 +14,14 @@ class RegisterController extends Controller
 
     public function store()
     {
-        User::create(request()->validate([
-            "name"=>["required","max:255"],
-            "username"=>["required","max:255","min:3"],
-            "email"=>["required","email","max:255"],
-            "password"=>["required", "min:7", "max:255"]
-        ]));
+        $attributes = request()->validate([
+            "name"     => ["required", "max:255"],
+            "username" => ["required", "max:255", "min:3"],
+            "email"    => ["required", "email", "max:255"],
+            "password" => ["required", "min:7", "max:255"]
+        ]);
+
+        User::create($attributes);
 
         return redirect("/");
     }
