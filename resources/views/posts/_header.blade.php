143f23ed90a16179072cabf06e5a07f7fbeb5c60
matthewproulx <matthewadrienproulx@gmail.com>
2022-01-07 11:24:53 -0800

    VID-42 Added search and filter category together options

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -46,7 +46,10 @@
     <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET"
-                  action="#">
+                  action="/">
+                @if (request("category"))
+                    <input type="hidden" name="category" value="{{request("category")}}"/>
+                @endif
                 <input type="text"
                        name="search"
                        placeholder="Find something"
