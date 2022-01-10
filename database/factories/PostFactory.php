66d8d80b23cbc60d405ecab1ab7697888df90d2b
matthewproulx <matthewadrienproulx@gmail.com>
2022-01-10 08:43:13 -0800

    VID-54 Added comments to actually fetch from the comment table and created a factory for them

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,8 +18,8 @@ class PostFactory extends Factory
         return [
             "user_id"     => User::factory(),
             "category_id" => Category::factory(),
-            "slug"        => $this->faker->slug,
-            "title"       => $this->faker->sentence,
+            "slug"        => $this->faker->slug(),
+            "title"       => $this->faker->sentence(),
             "excerpt"     => "<p>" . implode("</p><p>", $this->faker->paragraphs(2)) . "</p>",
             "body"     => "<p>" . implode("</p><p>", $this->faker->paragraphs(10)) . "</p>"
         ];
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,22 +1,21 @@
+@props(["comment"])
 <article class="flex bg-gray-100 p-6 border-2 border-gray-200 rounded-xl space-x-4">
     <div style="flex-shrink: 0">
-        <img src="https://i.pravatar.cc/60"
+        <img src="https://i.pravatar.cc/60?u={{$comment->id}}"
              class="rounded-xl"
              alt="">
     </div>
 
     <div>
         <header class="mb-4">
-            <h3 class="font bold">Madexio</h3>
+            <h3 class="font bold">{{$comment->author->username}}</h3>
             <p class="text-xs">
                 Posted
-                <time>8 months ago</time>
+                <time>{{$comment->created_at}}</time>
             </p>
         </header>
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            {{$comment->body}}
         </p>
     </div>
 </article>
