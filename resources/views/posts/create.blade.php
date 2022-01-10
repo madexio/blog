9a954f54ce6df3f246885831cbb80aa5c4f3bdb3
matthewproulx <matthewadrienproulx@gmail.com>
2022-01-10 15:22:40 -0800

    VID-63 Added an admin create post route

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,3 +1,99 @@
 <x-layout>
+    <section class="px-6 py-8">
+        <form action="/admin/posts"
+              method="post">
+            @csrf
+            <div class="border border-gray-200 p-6 rounded-xl max-w-md mx-auto">
+                <div class="mb-6 space-y-4">
+                    <div>
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                               for="title">
+                            Title
+                        </label>
+                        <input class="border border-gray-400 p-2 w-full"
+                               type="text"
+                               name="title"
+                               id="title"
+                               value="{{old('title')}}"
+                               required>
 
+                        @error("title")
+                        <p class="text-red-500 text-xs mt-2">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <div>
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                               for="slug">
+                            Slug
+                        </label>
+                        <input class="border border-gray-400 p-2 w-full"
+                               type="text"
+                               name="slug"
+                               id="slug"
+                               value="{{old('slug')}}"
+                               required>
+
+                        @error("slug")
+                        <p class="text-red-500 text-xs mt-2">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <div>
+                        <label class="block uppercase font-bold text-xs text-gray-700"
+                               for="excerpt">
+                            Excerpt
+                        </label>
+                        <textarea class="border border-gray-400 p-2 w-full"
+                                  name="excerpt"
+                                  id="excerpt"
+                                  rows="2"
+
+                                  required>{{old("excerpt")}}</textarea>
+                        @error("excerpt")
+                        <p class="text-red-500 text-xs mt-2">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <div>
+                        <label class="block uppercase font-bold text-xs text-gray-700"
+                               for="body">
+                            Body
+                        </label>
+                        <textarea class="border border-gray-400 p-2 w-full"
+                                  name="body"
+                                  id="body"
+                                  rows="10"
+                                  required>{{old("body")}}</textarea>
+                        @error("body")
+                        <p class="text-red-500 text-xs mt-2">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <div>
+                        <label class="block uppercase font-bold text-xs text-gray-700"
+                               for="category_id">
+                            Category
+                        </label>
+
+                        <select name="category_id"
+                                id="category_id">
+                            @php
+                                $categories = \App\Models\Category::all();
+                            @endphp
+                            @foreach($categories as $category)
+                                <option value="{{$category->id}}" {{old("category_id") == $category->id ? "selected" : ""}}>{{ucwords($category->name)}}</option>
+                            @endforeach
+                        </select>
+
+                        @error("category_id")
+                        <p class="text-red-500 text-xs mt-2">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <x-basic-button>Publish</x-basic-button>
+                </div>
+            </div>
+        </form>
+    </section>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,9 @@ Route::post("newsletter", NewsletterController::class);
 
 Route::get('/', [PostController::class, "index"])->name("home");
 Route::get("posts/{post:slug}", [PostController::class, "show"]);
+
 Route::get("admin/posts/create", [PostController::class, "create"])->middleware("admin");
+Route::post("admin/posts", [PostController::class, "store"])->middleware("admin");
 
 Route::post("posts/{post:slug}/comments", [PostCommentsController::class, "store"]);
 
